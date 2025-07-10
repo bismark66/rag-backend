7bc41ac364f6b2f695539fb947b16d76b51c0ed4
Bismark <agyarebismark6@gmail.com>
2025-07-10 11:02:43 +0000

    feat: Integrates all project dependencies

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { RagModule } from './rag/rag.module';
+import { DocumentsModule } from './documents/documents.module';
+import { ChatModule } from './chat/chat.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    RagModule,
+    DocumentsModule,
+    ChatModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
