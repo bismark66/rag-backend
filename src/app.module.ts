d65972931ea586e0c6a066a0fdca2a16f3bb8e7e
Bismark <agyarebismark6@gmail.com>
2025-08-28 20:33:32 +0000

    feat: import orm config and conversational module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,21 @@ import { AppService } from './app.service';
 import { RagModule } from './rag/rag.module';
 import { DocumentsModule } from './documents/documents.module';
 import { ChatModule } from './chat/chat.module';
+import { ConversationsModule } from './resources/conversations/conversations.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const ormsettings = require('../ormconfig');
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot(ormsettings),
     RagModule,
     DocumentsModule,
     ChatModule,
+    ConversationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
