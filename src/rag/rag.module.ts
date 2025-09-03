84345815e7b843b9be9ea8c345435085f926717a
Bismark <agyarebismark6@gmail.com>
2025-09-03 12:23:14 +0000

    feat: add api service and http module

--- src/rag/rag.module.ts
+++ src/rag/rag.module.ts
@@ -5,13 +5,16 @@ import { ConversationsService } from 'src/resources/conversations/conversations.
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Conversation } from 'src/db/entities/conversations.entity';
 import { Message } from 'src/db/entities/message.entity';
+import { ApiService } from 'src/common/utils/api.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     ConversationsModule,
     TypeOrmModule.forFeature([Conversation, Message]),
+    HttpModule,
   ],
-  providers: [RagService, ConversationsService],
+  providers: [RagService, ConversationsService, ApiService],
   exports: [RagService],
 })
 export class RagModule {}
